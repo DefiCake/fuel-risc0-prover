25289cfc0546535addf957cc6cc64d49a0c52053
DefiCake <rarore92@gmail.com>
2023-09-20 16:55:06 +0200

    chore: configure fuel-vm to work inside guest vm

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -5,12 +5,10 @@ use risc0_zkvm::guest::env;
 use sha2::{Digest, Sha256};
 use std::io::Read;
 risc0_zkvm::guest::entry!(main);
-use fuel_crypto::SecretKey;
 pub fn main() {
     let mut input = Vec::<u8>::new();
     env::stdin().read_to_end(&mut input).unwrap();
 
     let hash = Sha256::new().chain_update(&input).finalize().to_vec();
-    let secret_key = SecretKey::default();
     env::commit_slice(&ethabi::encode(&[Token::FixedBytes(hash)]));
 }
