547b96312b5015eb035de47d819b11f63397bc84
DefiCake <rarore92@gmail.com>
2023-10-13 13:39:26 +0200

    feat: add wip on database

--- core/src/database.rs
+++ core/src/database.rs
@@ -1,78 +1,78 @@
 
 pub struct Database {}
 
-// use serde::{
-//     de::DeserializeOwned,
-//     Serialize,
-// };
+use serde::{
+    de::DeserializeOwned,
+    Serialize,
+};
 
-// pub type Error;
-// pub type Result<T> = core::result::Result<T, Error>;
+type Error = anyhow::Error;
+type Result<T> = core::result::Result<T, Error>;
 
-// type DatabaseError = Error;
-// type DatabaseResult<T> = Result<T>;
+pub type DatabaseError = Error;
+pub type DatabaseResult<T> = Result<T>;
 
 
-// /// Database tables column ids to the corresponding [`fuel_core_storage::Mappable`] table.
-// #[repr(u32)]
-// #[derive(
-//     Copy, Clone, Debug, strum_macros::EnumCount, PartialEq, Eq, enum_iterator::Sequence,
-// )]
-// pub enum Column {
-//     /// The column id of metadata about the blockchain
-//     Metadata = 0,
-//     /// See [`ContractsRawCode`](fuel_core_storage::tables::ContractsRawCode)
-//     ContractsRawCode = 1,
-//     /// See [`ContractsInfo`](fuel_core_storage::tables::ContractsInfo)
-//     ContractsInfo = 2,
-//     /// See [`ContractsState`](fuel_core_storage::tables::ContractsState)
-//     ContractsState = 3,
-//     /// See [`ContractsLatestUtxo`](fuel_core_storage::tables::ContractsLatestUtxo)
-//     ContractsLatestUtxo = 4,
-//     /// See [`ContractsAssets`](fuel_core_storage::tables::ContractsAssets)
-//     ContractsAssets = 5,
-//     /// See [`Coins`](fuel_core_storage::tables::Coins)
-//     Coins = 6,
-//     /// The column of the table that stores `true` if `owner` owns `Coin` with `coin_id`
-//     OwnedCoins = 7,
-//     /// See [`Transactions`](fuel_core_storage::tables::Transactions)
-//     Transactions = 8,
-//     /// Transaction id to current status
-//     TransactionStatus = 9,
-//     /// The column of the table of all `owner`'s transactions
-//     TransactionsByOwnerBlockIdx = 10,
-//     /// See [`Receipts`](fuel_core_storage::tables::Receipts)
-//     Receipts = 11,
-//     /// See [`FuelBlocks`](fuel_core_storage::tables::FuelBlocks)
-//     FuelBlocks = 12,
-//     /// See [`FuelBlockSecondaryKeyBlockHeights`](storage::FuelBlockSecondaryKeyBlockHeights)
-//     FuelBlockSecondaryKeyBlockHeights = 13,
-//     /// See [`Messages`](fuel_core_storage::tables::Messages)
-//     Messages = 14,
-//     /// The column of the table that stores `true` if `owner` owns `Message` with `message_id`
-//     OwnedMessageIds = 15,
-//     /// See [`SealedBlockConsensus`](fuel_core_storage::tables::SealedBlockConsensus)
-//     FuelBlockConsensus = 16,
-//     /// See [`FuelBlockMerkleData`](storage::FuelBlockMerkleData)
-//     FuelBlockMerkleData = 17,
-//     /// See [`FuelBlockMerkleMetadata`](storage::FuelBlockMerkleMetadata)
-//     FuelBlockMerkleMetadata = 18,
-//     /// Messages that have been spent.
-//     /// Existence of a key in this column means that the message has been spent.
-//     /// See [`SpentMessages`](fuel_core_storage::tables::SpentMessages)
-//     SpentMessages = 19,
-//     /// Metadata for the relayer
-//     /// See [`RelayerMetadata`](fuel_core_relayer::ports::RelayerMetadata)
-//     RelayerMetadata = 20,
-//     /// See [`ContractsAssetsMerkleData`](storage::ContractsAssetsMerkleData)
-//     ContractsAssetsMerkleData = 21,
-//     /// See [`ContractsAssetsMerkleMetadata`](storage::ContractsAssetsMerkleMetadata)
-//     ContractsAssetsMerkleMetadata = 22,
-//     /// See [`ContractsStateMerkleData`](storage::ContractsStateMerkleData)
-//     ContractsStateMerkleData = 23,
-//     /// See [`ContractsStateMerkleMetadata`](storage::ContractsStateMerkleMetadata)
-//     ContractsStateMerkleMetadata = 24,
-// }
+/// Database tables column ids to the corresponding [`fuel_core_storage::Mappable`] table.
+#[repr(u32)]
+#[derive(
+    Copy, Clone, Debug, strum_macros::EnumCount, PartialEq, Eq, enum_iterator::Sequence,
+)]
+pub enum Column {
+    /// The column id of metadata about the blockchain
+    Metadata = 0,
+    /// See [`ContractsRawCode`](fuel_core_storage::tables::ContractsRawCode)
+    ContractsRawCode = 1,
+    /// See [`ContractsInfo`](fuel_core_storage::tables::ContractsInfo)
+    ContractsInfo = 2,
+    /// See [`ContractsState`](fuel_core_storage::tables::ContractsState)
+    ContractsState = 3,
+    /// See [`ContractsLatestUtxo`](fuel_core_storage::tables::ContractsLatestUtxo)
+    ContractsLatestUtxo = 4,
+    /// See [`ContractsAssets`](fuel_core_storage::tables::ContractsAssets)
+    ContractsAssets = 5,
+    /// See [`Coins`](fuel_core_storage::tables::Coins)
+    Coins = 6,
+    /// The column of the table that stores `true` if `owner` owns `Coin` with `coin_id`
+    OwnedCoins = 7,
+    /// See [`Transactions`](fuel_core_storage::tables::Transactions)
+    Transactions = 8,
+    /// Transaction id to current status
+    TransactionStatus = 9,
+    /// The column of the table of all `owner`'s transactions
+    TransactionsByOwnerBlockIdx = 10,
+    /// See [`Receipts`](fuel_core_storage::tables::Receipts)
+    Receipts = 11,
+    /// See [`FuelBlocks`](fuel_core_storage::tables::FuelBlocks)
+    FuelBlocks = 12,
+    /// See [`FuelBlockSecondaryKeyBlockHeights`](storage::FuelBlockSecondaryKeyBlockHeights)
+    FuelBlockSecondaryKeyBlockHeights = 13,
+    /// See [`Messages`](fuel_core_storage::tables::Messages)
+    Messages = 14,
+    /// The column of the table that stores `true` if `owner` owns `Message` with `message_id`
+    OwnedMessageIds = 15,
+    /// See [`SealedBlockConsensus`](fuel_core_storage::tables::SealedBlockConsensus)
+    FuelBlockConsensus = 16,
+    /// See [`FuelBlockMerkleData`](storage::FuelBlockMerkleData)
+    FuelBlockMerkleData = 17,
+    /// See [`FuelBlockMerkleMetadata`](storage::FuelBlockMerkleMetadata)
+    FuelBlockMerkleMetadata = 18,
+    /// Messages that have been spent.
+    /// Existence of a key in this column means that the message has been spent.
+    /// See [`SpentMessages`](fuel_core_storage::tables::SpentMessages)
+    SpentMessages = 19,
+    /// Metadata for the relayer
+    /// See [`RelayerMetadata`](fuel_core_relayer::ports::RelayerMetadata)
+    RelayerMetadata = 20,
+    /// See [`ContractsAssetsMerkleData`](storage::ContractsAssetsMerkleData)
+    ContractsAssetsMerkleData = 21,
+    /// See [`ContractsAssetsMerkleMetadata`](storage::ContractsAssetsMerkleMetadata)
+    ContractsAssetsMerkleMetadata = 22,
+    /// See [`ContractsStateMerkleData`](storage::ContractsStateMerkleData)
+    ContractsStateMerkleData = 23,
+    /// See [`ContractsStateMerkleMetadata`](storage::ContractsStateMerkleMetadata)
+    ContractsStateMerkleMetadata = 24,
+}
 
 // impl Column {
 //     /// The total count of variants in the enum.
