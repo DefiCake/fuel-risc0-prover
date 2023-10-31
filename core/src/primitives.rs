1b1ceb89558513e558b055b28d48660a8325df98
DefiCake <rarore92@gmail.com>
2023-10-31 12:48:31 +0100

    feat: add wip, dependencies for chain_config initialization

--- core/src/primitives.rs
+++ core/src/primitives.rs
@@ -1,10 +1,14 @@
 use derive_more::{
+    Add,
     AsRef,
+    Deref,
     Display,
     From,
     FromStr,
     Into,
     LowerHex,
+    Rem,
+    Sub,
     UpperHex,
 };
 
@@ -50,5 +54,70 @@ impl BlockId {
 impl AsRef<[u8]> for BlockId {
     fn as_ref(&self) -> &[u8] {
         self.as_slice()
+    }
+}
+
+/// Block height of the data availability layer
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(
+    Sub,
+    Copy,
+    Clone,
+    Debug,
+    Default,
+    PartialEq,
+    PartialOrd,
+    Eq,
+    Add,
+    Ord,
+    Display,
+    Into,
+    From,
+    Rem,
+    Deref,
+    Hash,
+)]
+#[rem(forward)]
+pub struct DaBlockHeight(pub u64);
+
+impl From<DaBlockHeight> for Vec<u8> {
+    fn from(height: DaBlockHeight) -> Self {
+        height.0.to_be_bytes().to_vec()
+    }
+}
+
+impl From<usize> for DaBlockHeight {
+    fn from(n: usize) -> Self {
+        DaBlockHeight(n as u64)
+    }
+}
+
+impl core::ops::Add<u64> for DaBlockHeight {
+    type Output = Self;
+
+    fn add(self, other: u64) -> Self::Output {
+        Self::from(self.0 + other)
+    }
+}
+
+impl DaBlockHeight {
+    /// Convert to array of big endian bytes
+    pub fn to_bytes(self) -> [u8; 8] {
+        self.0.to_be_bytes()
+    }
+
+    /// Convert to usize
+    pub fn to_usize(self) -> usize {
+        self.0 as usize
+    }
+
+    /// Convert to usize
+    pub fn as_usize(&self) -> usize {
+        self.0 as usize
+    }
+
+    /// Convert to u64
+    pub fn as_u64(&self) -> u64 {
+        self.0
     }
 }
