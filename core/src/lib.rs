2944b6f54af02be932f6c12074c438cfe7506b71
DefiCake <rarore92@gmail.com>
2023-12-12 12:25:36 +0100

    feat: implement check_transition

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -16,9 +16,14 @@ use std::sync::Arc;
 // use fuel_tx::Script;
 use fuel_core_chain_config::ChainConfig;
 use database::Database;
-use executor::{Executor, RelayerPort};
-use fuel_core_types::{blockchain::primitives::DaBlockHeight, entities::message::Message};
-use fuel_types::Nonce;
+use executor::{Executor, RelayerPort, OnceTransactionsSource};
+use fuel_core_types::{
+    blockchain::{primitives::DaBlockHeight, header::PartialBlockHeader}, 
+    entities::message::Message,
+    blockchain::{block::Block, primitives::BlockId}, services::{executor::ExecutionTypes, block_producer::Components}
+};
+use fuel_tx::{Script, Transaction};
+use fuel_types::{Nonce, Bytes32};
 
 #[derive(Clone, Debug)]
 pub struct MockRelayer {
@@ -33,11 +38,17 @@ impl RelayerPort for MockRelayer {
     }
 }
 
-pub fn initialize_interpreter(json: &str) -> Executor<MockRelayer> {   
-    let config: ChainConfig = serde_json::from_str(json).expect("Could not parse ChainConfig JSON");
+pub fn check_transition(
+    initial_chain_config_json: &str, 
+    target_block_json: &str, 
+    transaction_json: &str
+) -> BlockId {   
+    let config: ChainConfig = 
+        serde_json::from_str(initial_chain_config_json)
+        .expect("Could not parse ChainConfig JSON");
 
     let initial_state = config.clone().initial_state.expect("Could not load initial state");
-    let _initial_height = initial_state.height.expect("Could not load initial height");
+    let initial_height = initial_state.height.expect("Could not load initial height");
     let database = Database::in_memory();
     database.init(&config).expect("database.init() failed");
 
@@ -49,26 +60,51 @@ pub fn initialize_interpreter(json: &str) -> Executor<MockRelayer> {
         config: Arc::new(Default::default()),
     };
 
-    executor
+    let block: Block<Bytes32> = 
+        serde_json::from_str(target_block_json)
+        .expect("Could not parse target Block");
+
+    let time = block.header().time();
+
+    let height: fuel_crypto::fuel_types::BlockHeight = (u32::from(initial_height) + 1u32).into();
+    let prev_root = block.header().prev_root().clone();
+
+    let script_tx: Script = 
+        serde_json::from_str(transaction_json)
+        .expect("Could not deserialize script tx");
+    let transaction: Transaction = script_tx.into();
+
+    let mut def = PartialBlockHeader::default();
+    def.consensus.prev_root = prev_root;
+    def.consensus.time = time;
+    def.consensus.height = height;
+
+    let reproduced_block_header: PartialBlockHeader = PartialBlockHeader { ..def };
+
+    let component = ExecutionTypes::Production(Components {
+        header_to_produce: reproduced_block_header,
+        transactions_source: OnceTransactionsSource::new([transaction].into()),
+        gas_limit: u64::MAX
+    });    
+
+    let execution_result = executor.execute_without_commit(
+    component,
+    Default::default()
+    ).expect("Could not get execution result");
+
+    execution_result.result().block.header().hash()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::initialize_interpreter;
+    use crate::check_transition;
 
     const TEST_SNAPSHOT: &str = include_str!("../res/test_snapshot.json");
-
+    const TEST_BLOCK: &str = include_str!("../res/test_target_block.json");
+    const TEST_TRANSACTION: &str = include_str!("../res/test_transaction.json");
     #[test]
     fn test_initialize_interpreter() {
-
-        let _executor = initialize_interpreter(TEST_SNAPSHOT);        
-        // let interpreter = initialize_interpreter();
-
-        // let storage = interpreter.as_ref();
-
-        // let block_height = storage.block_height;
-
-        // assert_eq!(block_height, 0.into(), "Interpreter initialization failed");
+        let _block_id = check_transition(TEST_SNAPSHOT, TEST_BLOCK, TEST_TRANSACTION);
     }
 
     
