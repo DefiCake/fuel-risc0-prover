87a80a6c2b400cd1bda4e19e521c5bf72f282f99
DefiCake <rarore92@gmail.com>
2024-01-05 18:31:09 +0100

    feat: finish prototype

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 .PHONY: start start-relay
 
+# RISC0_BUILD_DEBUG=1 is a temporary workaround until https://github.com/risc0/risc0/pull/1257
+# ETA for this fix: v0.20.0-rc2
+# Meanwhile, build times will skyrocket
 start:
-	RUST_LOG=debug cargo run --bin host
+	RISC0_BUILD_DEBUG=1 RUST_LOG=debug cargo run --bin host
 start-relay:
-	RUST_LOG=debug cargo run --bin relay
+	RISC0_BUILD_DEBUG=1 RUST_LOG=debug cargo run --bin relay
 node-hardhat:
 	docker-compose up hardhat
 node:
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -2,7 +2,6 @@
 
 use ethabi::Token;
 use risc0_zkvm::guest::env;
-use sha2::{Digest, Sha256};
 use prover_core::{check_transition, Inputs};
 
 risc0_zkvm::guest::entry!(main);
@@ -10,12 +9,7 @@ risc0_zkvm::guest::entry!(main);
 pub fn main() {
     let Inputs {chain_config, target_block, transaction_json} = env::read();
 
-    let _block_id = check_transition(&chain_config, &target_block, &transaction_json);
-    
-    // let _interpreter: Interpreter<u64, fuel_tx::Script> = Default::default();
-    // let _interpreter = prover_core::initialize_interpreter();
+    let block_id = check_transition(&chain_config, &target_block, &transaction_json);
 
-    let hash = Sha256::new().chain_update(chain_config.as_bytes()).finalize().to_vec();
-
-    env::commit_slice(&ethabi::encode(&[Token::FixedBytes(hash)]));
+    env::commit_slice(&ethabi::encode(&[Token::FixedBytes(block_id.as_slice().to_vec())]));
 }
